0ea3606f4b9cd8eeae3870326265a934f3c01b53
Wojciech Kesek <wojtekesek@gmail.com>
2023-07-23 14:09:34 +0200

    init commit

--- src/main/java/com/example/springtask7b/config/ProductionConfig.java
+++ src/main/java/com/example/springtask7b/config/ProductionConfig.java
@@ -3,7 +3,6 @@ package com.example.springtask7b.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,10 +13,9 @@ public class ProductionConfig {
 
     @Bean
     public ObjectMapper objectMapper(){
-        JavaTimeModule module = new JavaTimeModule();
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
         objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CAMEL_CASE);
-        return objectMapper.registerModule(module);
+        return objectMapper;
     }
 }
